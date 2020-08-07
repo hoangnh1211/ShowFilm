8604a5522a76908d54eeee400180a2a908d278b5
hoangnh1211 <vuminhhoanganh12@gmail.com>
2020-08-07 11:09:24 +0700

    fix api

--- src/Api/Api.js
+++ src/Api/Api.js
@@ -1,6 +1,6 @@
 import Axios from "axios"
 export  function callApidata(id,type) {
-    const data = Axios.get(`http://www.omdbapi.com/?apikey=6b52ef7b&key=6b52ef7b&${type}=${id}`)
+    const data = Axios.get(`https://www.omdbapi.com/?apikey=6b52ef7b&key=6b52ef7b&${type}=${id}`)
     return data
   }
 
